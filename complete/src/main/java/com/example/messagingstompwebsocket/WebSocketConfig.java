598e2853499be86d54374cd181e9721647fa9110
ats3059 <ats3059@naver.com>
2022-05-27 22:55:15 +0900

    first commit

---
+++ complete/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		//토픽방식을 사용하겠다.
+		config.enableSimpleBroker("/topic");
+		// /app/~ 해당하는 path로 webSocket이 send 해야함 그러한 설정임 ㅇㅇ
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/gs-guide-websocket")
+				.withSockJS();
+	}
+
+}
---
+++ complete/src/main/resources/static/app.js
@@ -0,0 +1,56 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/gs-guide-websocket'); // -> 우리가 정해놓은 엔드포인트로 연결을 요청
+    stompClient = Stomp.over(socket); // STOMP는 SockJS 위에서 동작한다.
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        // /topic/greetings를 구독하겠다 -> 	@SendTo("/topic/greetings") 요기에 해당하는 메서드들에게 메시지를 받는 것
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    // /app/hello send하면 -> Controller에서 @MessageMapping("/hello") 가 붙은 메서드 실행
+    // stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+
+    const test = JSON.stringify({'name': $("#name").val()});
+    stompClient.send("/app/hello", {}, test);
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
